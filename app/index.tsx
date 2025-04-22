9f1c17f4cc578c2c8fe8f221a57b3e1d0c8061c6
ivanleopoldo <ivanleopoldoc@gmail.com>
2025-04-22 19:07:09 +0800

    feat: connects db

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,25 @@
+import { supabase } from '@/utils/supabase';
 import { Stack, Link } from 'expo-router';
+import React from 'react';
 import { Text, View } from 'react-native';
 
 export default function Home() {
+  React.useEffect(() => {
+    getTodos();
+  }, []);
+
+  const getTodos = async () => {
+    const { data, error } = await supabase.from('todos').select('*');
+
+    if (data) {
+      console.log(data);
+    }
+
+    if (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <>
       <Stack.Screen options={{ title: 'Home' }} />
